c63357c52fa5d4284e55a76564c9bfed4076936d
Tim Borovkov <tim.borovkov@icloud.com>
2025-01-06 15:49:47 +0200

    Update role.go

--- models/role.go
+++ models/role.go
@@ -1,7 +1,11 @@
 package models
 
+// IrminRole represents a role object in the system.
 type IrminRole struct {
+	// Human-readable description
 	Description string `json:"description"`
-	Label       string `json:"label"`
-	Name        string `json:"name"`
+	// Human-readable name
+	Label string `json:"label"`
+	// Slug of the role, e.g., 'admin', 'editor', 'billing', 'viewer', etc.
+	Name string `json:"name"`
 }
