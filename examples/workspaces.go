8dd3ac950e27260d9029cbace99671bc04482998
Tim Borovkov <tim.borovkov@icloud.com>
2025-01-09 12:37:27 +0100

    Update workspaces.go

--- examples/workspaces.go
+++ examples/workspaces.go
@@ -30,6 +30,31 @@ func TestWorkspaces() {
 	apiClient := client.NewClient(baseURL, apiToken, locale)
 	workspaceService := services.NewWorkspaceService(apiClient)
 
+	// Create a new workspace
+	workspace, res, err := workspaceService.CreateWorkspace("Test Workspace", "This is for SDK testing")
+	if err != nil {
+		fmt.Println("Error creating workspace:", err)
+		return
+	}
+	fmt.Println(res.Message)
+	fmt.Printf("Created workspace: %s (%s)\n", workspace.Name, workspace.Slug)
+
+	// Switch to the new workspace
+	res, err = workspaceService.SwitchWorkspace(workspace.Slug)
+	if err != nil {
+		fmt.Println("Error switching workspace:", err)
+		return
+	}
+	fmt.Println(res.Message)
+
+	// Delete the new workspace
+	res, err = workspaceService.DeleteWorkspace(workspace.Slug)
+	if err != nil {
+		fmt.Println("Error deleting workspace:", err)
+		return
+	}
+	fmt.Println(res.Message)
+
 	// Fetch workspaces
 	workspaces, _, err := workspaceService.FetchWorkspaces()
 	if err != nil {
