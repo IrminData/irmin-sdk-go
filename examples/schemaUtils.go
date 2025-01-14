d65e3663b250d6bcfb4c8de0114b3491dd1c1fa7
Tim Borovkov <tim.borovkov@icloud.com>
2025-01-14 02:09:28 +0100

    Tim/feat/go struct to json schema (#15)
    * Add invopop/jsonschema
    
    * JSONSchemaFromStruct
    
    * Add json schema creation to TestSchemaUtils
    
    * Update schemaUtils.go
    
    * Update schemaUtils.go
    
    * Resolve any "$ref" in the schema
    
    * Tidy

--- examples/schemaUtils.go
+++ examples/schemaUtils.go
@@ -1,35 +1,37 @@
 package examples
 
 import (
-	"encoding/json"
 	"fmt"
 	"irmin-sdk/utils"
 )
 
-func TestSchemaUtils() {
-	// Example JSON schema
-	const exampleJSONSchema = `{
-		"type": "object",
-		"properties": {
-		  "Name": { "type": "string" },
-		  "Age": { "type": "integer" },
-		  "Scores": {
-			"type": "array",
-			"items": { "type": "number" }
-		  }
-		},
-		"required": ["Name", "Age"]
-	  }`
+// Example struct for testing schema operations.
+type TestUser struct {
+	ID          int                    `json:"id"`
+	Name        string                 `json:"name" jsonschema:"title=the name,description=The name of a friend,example=joe,example=lucy,default=alex"`
+	Friends     []int                  `json:"friends,omitempty" jsonschema_description:"The list of IDs, omitted when empty"`
+	Tags        map[string]interface{} `json:"tags,omitempty" jsonschema_extras:"a=b,foo=bar,foo=bar1"`
+	BirthDate   string                 `json:"birth_date,omitempty" jsonschema:"oneof_required=date"`
+	YearOfBirth string                 `json:"year_of_birth,omitempty" jsonschema:"oneof_required=year"`
+	Metadata    interface{}            `json:"metadata,omitempty" jsonschema:"oneof_type=string;array"`
+	FavColour   string                 `json:"fav_color,omitempty" jsonschema:"enum=red,enum=green,enum=blue"`
+}
 
-	var schema map[string]interface{}
-	if err := json.Unmarshal([]byte(exampleJSONSchema), &schema); err != nil {
-		fmt.Println("Error unmarshalling JSON schema:", err)
+// TestSchemaUtils tests the schema utilities in the SDK.
+func TestSchemaUtils() {
+	// Create JSON schema from struct
+	fmt.Println("Testing JSONSchemaFromStruct...")
+	schemaBytes, schemaMap, err := utils.JSONSchemaFromStruct(TestUser{}, "test_user")
+	if err != nil {
+		fmt.Println("Error:", err)
 		return
 	}
+	fmt.Println("JSON schema:")
+	fmt.Println(string(schemaBytes))
 
 	// Convert JSON schema to Parquet schema
 	fmt.Println("Testing JSONSchemaToParquet...")
-	parquetSchema := utils.JSONSchemaToParquet(schema, "example_root")
+	parquetSchema := utils.JSONSchemaToParquet(schemaMap, "example_root")
 	fmt.Println("Parquet schema:")
 	fmt.Println(parquetSchema)
 }
