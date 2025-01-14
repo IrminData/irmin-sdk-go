5394ea6dfd369612a17d7a66b6b2e4ce85cde1b4
Tim Borovkov <tim.borovkov@icloud.com>
2025-01-14 01:59:58 +0100

    Update schemaUtils.go

--- examples/schemaUtils.go
+++ examples/schemaUtils.go
@@ -14,9 +14,10 @@ type TestUser struct {
 	BirthDate   string                 `json:"birth_date,omitempty" jsonschema:"oneof_required=date"`
 	YearOfBirth string                 `json:"year_of_birth,omitempty" jsonschema:"oneof_required=year"`
 	Metadata    interface{}            `json:"metadata,omitempty" jsonschema:"oneof_type=string;array"`
-	FavColour   string                 `json:"fav_color,omitempty" jsonschema:"enum=red,enum=green,enum=blue"` // British spelling
+	FavColour   string                 `json:"fav_color,omitempty" jsonschema:"enum=red,enum=green,enum=blue"`
 }
 
+// TestSchemaUtils tests the schema utilities in the SDK.
 func TestSchemaUtils() {
 	// Create JSON schema from struct
 	fmt.Println("Testing JSONSchemaFromStruct...")
