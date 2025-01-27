55c0dbf6ac9883450aa1880cf1ffbf5233068282
Tim Borovkov <tim.borovkov@icloud.com>
2025-01-27 19:36:35 +0100

    Define JSON Patch model

---
+++ models/jsonPatch.go
@@ -0,0 +1,16 @@
+package models
+
+// PatchOperation represents a single operation in a JSON Patch array.
+type PatchOperation struct {
+	// The operation: "add", "remove", "replace", "move" or "copy"
+	Op string `json:"op"`
+	// The JSON-Pointer location to apply the operation
+	Path string `json:"path"`
+	// Used for "move" or "copy" operations
+	From string `json:"from,omitempty"`
+	// Used for "add" or "replace" operations
+	Value interface{} `json:"value,omitempty"`
+}
+
+// Patch is a series of patch operations.
+type Patch []PatchOperation
