a93f1fc4c3634361ed6ee6054ccc375809909b07
Tim Borovkov <tim.borovkov@icloud.com>
2025-01-29 15:33:52 +0100

    Define ConnectorEvent model

---
+++ models/connectorEvent.go
@@ -0,0 +1,11 @@
+package models
+
+// ConnectorEvent represents a webhook event sent by a connector when a change in the data occurs.
+type ConnectorEvent struct {
+	// Type of the event (e.g. "create", "update", "delete")
+	Type string `json:"type"`
+	// Irmin path of the event (e.g. /maindb/users.json/1/name)
+	Path string `json:"path"`
+	// Timestamp of the event in milliseconds since the Unix epoch
+	Timestamp int64 `json:"timestamp"`
+}
