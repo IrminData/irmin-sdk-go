a3e4bb30786131c285b41ac7962462fcc889a1f3
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-26 15:35:14 -0700

    Update workflow.go

--- core-api/workflow.go
+++ core-api/workflow.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"maps"
+
 	irminModels "github.com/IrminData/irmin-sdk-go/models"
 	irminUtils "github.com/IrminData/irmin-sdk-go/utils"
 )
@@ -56,6 +58,40 @@ func (s *WorkflowService) GetWorkflow(workspace, workflowID string) (*irminModel
 	return &workflow, apiResp, nil
 }
 
+func (s *WorkflowService) CreateWorkflow(workspace, name, description, documentation string, workflowable irminModels.Workflowable, schedule irminModels.Schedule) (*irminModels.Workflow, *irminModels.IrminAPIResponse, error) {
+	fields := map[string]string{
+		"name":          name,
+		"description":   description,
+		"documentation": documentation,
+	}
+	// Add schedule data
+	scheduleFields, err := irminUtils.PrepareWorkflowScheduleData(schedule)
+	if err != nil {
+		return nil, nil, fmt.Errorf("prepare schedule data error: %w", err)
+	}
+	maps.Copy(fields, scheduleFields)
+
+	// Add workflowable data
+	worklowableFields, err := irminUtils.PrepareWorkflowableData(workflowable)
+	if err != nil {
+		return nil, nil, fmt.Errorf("prepare workflowable data error: %w", err)
+	}
+	maps.Copy(fields, worklowableFields)
+
+	// Create the workflow
+	var workflow irminModels.Workflow
+	apiResp, err := s.client.FetchAPI(RequestOptions{
+		Method:      http.MethodPost,
+		Endpoint:    fmt.Sprintf("/v1/workspaces/%s/workflows", workspace),
+		ContentType: "application/x-www-form-urlencoded",
+		FormFields:  fields,
+	}, &workflow)
+	if err != nil {
+		return nil, nil, fmt.Errorf("create workflow error: %w", err)
+	}
+	return &workflow, apiResp, nil
+}
+
 func (s *WorkflowService) UpdateWorkflow(workspace, workflowID, name, description, documentation string) (*irminModels.Workflow, *irminModels.IrminAPIResponse, error) {
 	var workflow irminModels.Workflow
 	apiResp, err := s.client.FetchAPI(RequestOptions{
@@ -70,6 +106,24 @@ func (s *WorkflowService) UpdateWorkflow(workspace, workflowID, name, descriptio
 	}, &workflow)
 	if err != nil {
 		return nil, nil, fmt.Errorf("update workflow error: %w", err)
+	}
+	return &workflow, apiResp, nil
+}
+
+func (s *WorkflowService) UpdateWorkflowWorkflowable(workspace, workflowID string, workflowable irminModels.Workflowable) (*irminModels.Workflow, *irminModels.IrminAPIResponse, error) {
+	workflowableFields, err := irminUtils.PrepareWorkflowableData(workflowable)
+	if err != nil {
+		return nil, nil, fmt.Errorf("prepare workflowable data error: %w", err)
+	}
+	var workflow irminModels.Workflow
+	apiResp, err := s.client.FetchAPI(RequestOptions{
+		Method:      http.MethodPatch,
+		Endpoint:    fmt.Sprintf("/v1/workspaces/%s/workflows/%s/workflowable", workspace, workflowID),
+		ContentType: "application/x-www-form-urlencoded",
+		FormFields:  workflowableFields,
+	}, &workflow)
+	if err != nil {
+		return nil, nil, fmt.Errorf("update workflow workflowable error: %w", err)
 	}
 	return &workflow, apiResp, nil
 }
