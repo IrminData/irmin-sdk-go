1f12712679f086236020f6c52541da3a109831c0
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-25 21:20:44 -0700

    Update prepareWorkflowScheduleData.go

--- utils/prepareWorkflowScheduleData.go
+++ utils/prepareWorkflowScheduleData.go
@@ -9,49 +9,44 @@ import (
 
 // PrepareWorkflowScheduleData prepares a map of fields for a workflow schedule.
 // Returns an array of fields to be used in a form submission
-func PrepareWorkflowScheduleData(schedule irminModels.WorkflowSchedule) (map[string]string, error) {
+func PrepareWorkflowScheduleData(schedule irminModels.Schedule) (map[string]string, error) {
 	fields := make(map[string]string)
 
 	for index, trigger := range schedule.Triggers {
 		fieldPrefix := "trigger[" + strconv.Itoa(index) + "]."
 
 		// Type assertion to access concrete fields
-		switch t := trigger.(type) {
-		case *irminModels.TimeTrigger:
+		switch trigger.Type {
+		case irminModels.TimeTriggerType:
 			// Write time trigger fields
 			fields[fieldPrefix+"type"] = "time"
-			fields[fieldPrefix+"rrule"] = t.RRule
-		case *irminModels.RepositoryTrigger:
+			fields[fieldPrefix+"rrule"] = *trigger.RRule
+			fields[fieldPrefix+"cron"] = *trigger.Cron
+		case irminModels.RepositoryTriggerType:
 			// Write repository trigger fields
 			fields[fieldPrefix+"type"] = "repository-event"
-			fields[fieldPrefix+"event"] = string(t.Event)
-			if t.Repository != nil {
-				fields[fieldPrefix+"repository"] = *t.Repository
-			}
-			if t.Ref != nil {
-				fields[fieldPrefix+"ref"] = *t.Ref
-			}
-		case *irminModels.WorkflowRunTrigger:
+			fields[fieldPrefix+"event"] = string(*trigger.RepositoryEvent)
+			fields[fieldPrefix+"repository"] = *trigger.Repository
+			fields[fieldPrefix+"branch"] = *trigger.RepositoryRef
+		case irminModels.WorkflowRunTriggerType:
 			// Write workflow run trigger fields
 			fields[fieldPrefix+"type"] = "workflow-run-event"
-			fields[fieldPrefix+"event"] = string(t.Event)
-			if t.Workflow != nil {
-				fields[fieldPrefix+"workflow"] = *t.Workflow
-			}
+			fields[fieldPrefix+"event"] = string(*trigger.WorkflowRunEvent)
+			fields[fieldPrefix+"workflow"] = *trigger.WorkflowID
 		default:
 			return nil, fmt.Errorf("unknown trigger type at index %d", index)
 		}
 	}
 
 	// Write optional schedule fields
-	if schedule.MaxRetries != nil {
-		fields["max_retries"] = strconv.Itoa(*schedule.MaxRetries)
+	if schedule.MaxRetries > 0 {
+		fields["max_retries"] = strconv.Itoa(schedule.MaxRetries)
 	}
-	if schedule.MaxRuntime != nil {
-		fields["max_runtime"] = strconv.Itoa(*schedule.MaxRuntime)
+	if schedule.MaxRuntime > 0 {
+		fields["max_runtime"] = strconv.Itoa(schedule.MaxRuntime)
 	}
-	if schedule.MinInterval != nil {
-		fields["min_interval"] = strconv.Itoa(*schedule.MinInterval)
+	if schedule.MinInterval > 0 {
+		fields["min_interval"] = strconv.Itoa(schedule.MinInterval)
 	}
 
 	return fields, nil
