5cd83a669a8bbe55ec977e501ba488e4e969351a
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-25 22:39:43 -0700

    Update object.go

--- core-api/object.go
+++ core-api/object.go
@@ -20,65 +20,35 @@ func NewObjectService(client *Client) *ObjectService {
 	}
 }
 
-// FetchObjects retrieves objects at a given path in a repository and ref
-func (s *ObjectService) FetchObjects(repository, path, ref string) ([]irminModels.Object, *irminModels.IrminAPIResponse, error) {
-	// Build the endpoint: /v1/repositories/:repository/objects/:path removing the first / from path if it exists
-	if len(path) > 0 && path[0] == '/' {
-		path = path[1:]
-	}
-	endpoint := fmt.Sprintf("/v1/repositories/%s/objects/%s", repository, path)
-
-	// Add ref query parameter if provided
-	if ref != "" {
-		endpoint += fmt.Sprintf("?ref=%s", ref)
-	}
-
-	var objects []irminModels.Object
+func (s *ObjectService) GetObjectAtPath(workspace, repository, path, ref string) (*irminModels.Object, *irminModels.IrminAPIResponse, error) {
+	var objects irminModels.Object
 	apiResp, err := s.client.FetchAPI(RequestOptions{
 		Method:   http.MethodGet,
-		Endpoint: endpoint,
+		Endpoint: fmt.Sprintf("/v1/workspaces/%s/repositories/%s/objects?ref=%s&path=%s", workspace, repository, ref, path),
 	}, &objects)
 	if err != nil {
 		return nil, nil, fmt.Errorf("fetch objects error: %w", err)
 	}
-	return objects, apiResp, nil
+	return &objects, apiResp, nil
 }
 
-// FetchObject retrieves a single object by its name and path in a repository
-func (s *ObjectService) FetchObject(repository, path, ref string) (*irminModels.Object, *irminModels.IrminAPIResponse, error) {
-	// Build the endpoint: /v1/repositories/:repository/objects/:path removing the first / from path if it exists
-	if len(path) > 0 && path[0] == '/' {
-		path = path[1:]
-	}
-	endpoint := fmt.Sprintf("/v1/repositories/%s/objects/%s", repository, path)
-
-	// Add ref query parameter if provided
-	if ref != "" {
-		endpoint += fmt.Sprintf("?ref=%s", ref)
-	}
-
-	var object irminModels.Object
+func (s *ObjectService) GetObjectHistory(workspace, repository, path, ref string) ([]irminModels.Commit, *irminModels.IrminAPIResponse, error) {
+	var commits []irminModels.Commit
 	apiResp, err := s.client.FetchAPI(RequestOptions{
 		Method:   http.MethodGet,
-		Endpoint: endpoint,
-	}, &object)
+		Endpoint: fmt.Sprintf("/v1/workspaces/%s/repositories/%s/objects/history?ref=%s&path=%s", workspace, repository, ref, path),
+	}, &commits)
 	if err != nil {
-		return nil, nil, fmt.Errorf("fetch object error: %w", err)
+		return nil, nil, fmt.Errorf("fetch object history error: %w", err)
 	}
-	return &object, apiResp, nil
+	return commits, apiResp, nil
 }
 
-// FetchObjectSchema retrieves the schema of an object in a repository
-func (s *ObjectService) FetchObjectSchema(repository, path, ref string) (*irminModels.ObjectSchema, *irminModels.IrminAPIResponse, error) {
-	endpoint := fmt.Sprintf("/v1/repositories/%s/objects/schema/%s", repository, path)
-	if ref != "" {
-		endpoint += fmt.Sprintf("?ref=%s", ref)
-	}
-
+func (s *ObjectService) GetObjectSchema(workspace, repository, path, ref string) (*irminModels.ObjectSchema, *irminModels.IrminAPIResponse, error) {
 	var schema irminModels.ObjectSchema
 	apiResp, err := s.client.FetchAPI(RequestOptions{
 		Method:   http.MethodGet,
-		Endpoint: endpoint,
+		Endpoint: fmt.Sprintf("/v1/workspaces/%s/repositories/%s/objects/schema?ref=%s&path=%s", workspace, repository, ref, path),
 	}, &schema)
 	if err != nil {
 		return nil, nil, fmt.Errorf("fetch object schema error: %w", err)
@@ -86,42 +56,7 @@ func (s *ObjectService) FetchObjectSchema(repository, path, ref string) (*irminM
 	return &schema, apiResp, nil
 }
 
-// FetchContent retrieves the content of an object at a given path
-func (s *ObjectService) FetchContent(repository, path, ref string, raw bool) ([]byte, error) {
-	endpoint := fmt.Sprintf("/v1/repositories/%s/objects/content/%s", repository, path)
-	if ref != "" {
-		endpoint += fmt.Sprintf("?ref=%s", ref)
-	}
-	if raw {
-		endpoint += "&raw=true"
-	}
-
-	apiResp, err := s.client.FetchBinary(RequestOptions{
-		Method:   http.MethodGet,
-		Endpoint: endpoint,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("fetch content error: %w", err)
-	}
-	return apiResp, nil
-}
-
-// UploadObject creates or updates an object in the repository
-func (s *ObjectService) UploadObject(
-	repository string,
-	ref string,
-	path string,
-	name string,
-	files map[string][]byte,
-) (*irminModels.Object, *irminModels.IrminAPIResponse, error) {
-
-	// Build the endpoint: /v1/repositories/:repository/objects/:path removing the first / from path if it exists
-	if len(path) > 0 && path[0] == '/' {
-		path = path[1:]
-	}
-	endpoint := fmt.Sprintf("/v1/repositories/%s/objects/%s", repository, path)
-
-	// Convert files map into []FormFile
+func (s *ObjectService) UploadObject(workspace, repository, ref, path, name string, files map[string][]byte) (*irminModels.Object, *irminModels.IrminAPIResponse, error) {
 	var formFiles []FormFile
 	for fileName, fileContent := range files {
 		// Use bytes.NewReader for in-memory file data
@@ -133,25 +68,13 @@ func (s *ObjectService) UploadObject(
 		})
 	}
 
-	// Build form fields for non-file data
-	formFields := map[string]string{
-		"ref": ref,
-	}
-
-	// Construct your RequestOptions for a multipart form upload
-	reqOpts := RequestOptions{
+	var object irminModels.Object
+	apiResp, err := s.client.FetchAPI(RequestOptions{
 		Method:      http.MethodPost,
-		Endpoint:    endpoint,
-		FormFields:  formFields,
+		Endpoint:    fmt.Sprintf("/v1/workspaces/%s/repositories/%s/objects?ref=%s&path=%s", workspace, repository, ref, path),
 		Files:       formFiles,
 		ContentType: "multipart/form-data",
-	}
-
-	// Prepare an object to hold the response data
-	var object irminModels.Object
-
-	// FetchAPI will also parse the IrminAPIResponse
-	apiResp, err := s.client.FetchAPI(reqOpts, &object)
+	}, &object)
 	if err != nil {
 		return nil, nil, fmt.Errorf("upload object error: %w", err)
 	}
@@ -159,27 +82,26 @@ func (s *ObjectService) UploadObject(
 	return &object, apiResp, nil
 }
 
-// MoveObject moves or renames an object in the repository
-func (s *ObjectService) MoveObject(repository, ref, path, newPath, newName string) (*irminModels.Object, *irminModels.IrminAPIResponse, error) {
-	form := map[string]string{
-		"_method":  "MOVE",
-		"ref":      ref,
-		"new_path": newPath,
-		"new_name": newName,
+func (s *ObjectService) GetObjectContent(workspace, repository, path, ref string) ([]byte, error) {
+	apiResp, err := s.client.FetchBinary(RequestOptions{
+		Method:   http.MethodGet,
+		Endpoint: fmt.Sprintf("/v1/workspaces/%s/repositories/%s/objects/content?ref=%s&path=%s", workspace, repository, ref, path),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("fetch content error: %w", err)
 	}
+	return apiResp, nil
+}
 
-	// Build the endpoint: /v1/repositories/:repository/objects/:path removing the first / from path if it exists
-	if len(path) > 0 && path[0] == '/' {
-		path = path[1:]
-	}
-	endpoint := fmt.Sprintf("/v1/repositories/%s/objects/%s", repository, path)
-
+func (s *ObjectService) MoveObject(workspace, repository, path, ref, newPath string) (*irminModels.Object, *irminModels.IrminAPIResponse, error) {
 	var object irminModels.Object
 	apiResp, err := s.client.FetchAPI(RequestOptions{
 		Method:      http.MethodPost,
-		Endpoint:    endpoint,
+		Endpoint:    fmt.Sprintf("/v1/workspaces/%s/repositories/%s/objects/move?ref=%s&path=%s", workspace, repository, ref, path),
 		ContentType: "application/x-www-form-urlencoded",
-		FormFields:  form,
+		FormFields: map[string]string{
+			"new_path": newPath,
+		},
 	}, &object)
 	if err != nil {
 		return nil, nil, fmt.Errorf("move object error: %w", err)
@@ -187,24 +109,26 @@ func (s *ObjectService) MoveObject(repository, ref, path, newPath, newName strin
 	return &object, apiResp, nil
 }
 
-// DeleteObject deletes an object from the repository
-func (s *ObjectService) DeleteObject(repository, ref, path, name string) (*irminModels.IrminAPIResponse, error) {
-	form := map[string]string{
-		"_method": "DELETE",
-		"ref":     ref,
-	}
-
-	// Build the endpoint: /v1/repositories/:repository/objects/:path removing the first / from path if it exists
-	if len(path) > 0 && path[0] == '/' {
-		path = path[1:]
-	}
-	endpoint := fmt.Sprintf("/v1/repositories/%s/objects/%s", repository, path)
-
+func (s *ObjectService) CopyObject(workspace, repository, path, ref, newPath string) (*irminModels.Object, *irminModels.IrminAPIResponse, error) {
+	var object irminModels.Object
 	apiResp, err := s.client.FetchAPI(RequestOptions{
 		Method:      http.MethodPost,
-		Endpoint:    endpoint,
+		Endpoint:    fmt.Sprintf("/v1/workspaces/%s/repositories/%s/objects/copy?ref=%s&path=%s", workspace, repository, ref, path),
 		ContentType: "application/x-www-form-urlencoded",
-		FormFields:  form,
+		FormFields: map[string]string{
+			"new_path": newPath,
+		},
+	}, &object)
+	if err != nil {
+		return nil, nil, fmt.Errorf("copy object error: %w", err)
+	}
+	return &object, apiResp, nil
+}
+
+func (s *ObjectService) DeleteObject(workspace, repository, ref, path string) (*irminModels.IrminAPIResponse, error) {
+	apiResp, err := s.client.FetchAPI(RequestOptions{
+		Method:   http.MethodDelete,
+		Endpoint: fmt.Sprintf("/v1/workspaces/%s/repositories/%s/objects?ref=%s&path=%s", workspace, repository, ref, path),
 	}, nil)
 	if err != nil {
 		return nil, fmt.Errorf("delete object error: %w", err)
