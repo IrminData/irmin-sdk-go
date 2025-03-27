9f7068e4c361438cd5a6da74e05100d6c87d0c58
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-26 23:13:17 -0700

    Update credentials.go

--- core-api/credentials.go
+++ core-api/credentials.go
@@ -8,21 +8,9 @@ import (
 	irminModels "github.com/IrminData/irmin-sdk-go/models"
 )
 
-// CredentialService handles operations related to system tokens
-type CredentialService struct {
-	client *Client
-}
-
-// NewCredentialService creates a new instance of CredentialService
-func NewCredentialService(client *Client) *CredentialService {
-	return &CredentialService{
-		client: client,
-	}
-}
-
-func (s *CredentialService) ListTokens() ([]irminModels.APIToken, *irminModels.IrminAPIResponse, error) {
+func (c *Client) ListTokens() ([]irminModels.APIToken, *irminModels.IrminAPIResponse, error) {
 	var tokens []irminModels.APIToken
-	apiResp, err := s.client.FetchAPI(RequestOptions{
+	apiResp, err := c.FetchAPI(RequestOptions{
 		Method:   http.MethodGet,
 		Endpoint: "/v1/credentials",
 	}, &tokens)
@@ -32,9 +20,9 @@ func (s *CredentialService) ListTokens() ([]irminModels.APIToken, *irminModels.I
 	return tokens, apiResp, nil
 }
 
-func (s *CredentialService) CreateToken(name string, expiry int) (*irminModels.APIToken, *irminModels.IrminAPIResponse, error) {
+func (c *Client) CreateToken(name string, expiry int) (*irminModels.APIToken, *irminModels.IrminAPIResponse, error) {
 	var token irminModels.APIToken
-	apiResp, err := s.client.FetchAPI(RequestOptions{
+	apiResp, err := c.FetchAPI(RequestOptions{
 		Method:      http.MethodPost,
 		Endpoint:    "/v1/credentials",
 		ContentType: "application/x-www-form-urlencoded",
@@ -49,8 +37,8 @@ func (s *CredentialService) CreateToken(name string, expiry int) (*irminModels.A
 	return &token, apiResp, nil
 }
 
-func (s *CredentialService) DeleteToken(tokenID string) (*irminModels.IrminAPIResponse, error) {
-	apiResp, err := s.client.FetchAPI(RequestOptions{
+func (c *Client) DeleteToken(tokenID string) (*irminModels.IrminAPIResponse, error) {
+	apiResp, err := c.FetchAPI(RequestOptions{
 		Method:      http.MethodDelete,
 		Endpoint:    fmt.Sprintf("/v1/credentials/%s", tokenID),
 		ContentType: "application/x-www-form-urlencoded",
