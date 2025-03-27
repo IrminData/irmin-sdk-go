1ae3563268575bac27d8fcfc659ffcaf366e3221
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-26 23:13:25 -0700

    Update log.go

--- core-api/log.go
+++ core-api/log.go
@@ -7,22 +7,10 @@ import (
 	irminModels "github.com/IrminData/irmin-sdk-go/models"
 )
 
-// LogService handles log-related API calls
-type LogService struct {
-	client *Client
-}
-
-// NewLogService creates a new LogService
-func NewLogService(client *Client) *LogService {
-	return &LogService{
-		client: client,
-	}
-}
-
 // FetchLogEvents retrieves general audit log events for the current workspace
-func (s *LogService) FetchLogEvents(workspace string) ([]irminModels.LogEvent, *irminModels.IrminAPIResponse, error) {
+func (c *Client) FetchLogEvents(workspace string) ([]irminModels.LogEvent, *irminModels.IrminAPIResponse, error) {
 	var logEvents []irminModels.LogEvent
-	apiResp, err := s.client.FetchAPI(RequestOptions{
+	apiResp, err := c.FetchAPI(RequestOptions{
 		Method:   http.MethodGet,
 		Endpoint: fmt.Sprintf("/v1/workspaces/%s/logs", workspace),
 	}, &logEvents)
