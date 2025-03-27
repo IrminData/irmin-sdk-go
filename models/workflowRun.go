438b445c6a95fe47bfc5d2920f64c08f07e00859
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-27 13:50:30 -0700

    Add more timestamps for WorkflowRun type (#43)

--- models/workflowRun.go
+++ models/workflowRun.go
@@ -5,6 +5,9 @@ import "time"
 type WorkflowRun struct {
 	ID              string           `json:"id"`
 	CreatedAt       time.Time        `json:"created_at"`
+	UpdatedAt       time.Time        `json:"updated_at"`
+	StartedAt       *time.Time       `json:"started_at,omitempty"`
+	FinishedAt      *time.Time       `json:"finished_at,omitempty"`
 	Status          WorkflowStatus   `json:"status"`
 	TriggeredBy     *ScheduleTrigger `json:"triggered_by,omitempty"`
 	TriggeredByUser *User            `json:"triggered_by_user,omitempty"`
