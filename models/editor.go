de80ca6cb65f58a227950f2a3d6f104a7f713023
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-31 02:33:41 -0700

    Expand editor item type

--- models/editor.go
+++ models/editor.go
@@ -3,9 +3,11 @@ package irminModels
 import "time"
 
 type EditorItem struct {
-	Name         string    `json:"name"`
-	Path         string    `json:"path"`
-	Type         string    `json:"type"` // file or folder
-	Content      *string   `json:"content,omitempty"`
-	LastModified time.Time `json:"last_modified"`
+	Name         string       `json:"name"`
+	Path         string       `json:"path"`
+	Type         string       `json:"type"` // file or folder
+	Content      *string      `json:"content,omitempty"`
+	Language     *string      `json:"language,omitempty"` // js, py, go, etc.
+	Children     []EditorItem `json:"children,omitempty"` // for folders
+	LastModified time.Time    `json:"last_modified"`
 }
