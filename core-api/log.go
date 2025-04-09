19e995d343dba0549ece97d8689285d19f9ccd6d
Tim Borovkov <tim.borovkov@icloud.com>
2025-04-09 13:31:07 -0700

    FetchLogEventsForRepository

--- core-api/log.go
+++ core-api/log.go
@@ -19,3 +19,16 @@ func (c *Client) FetchLogEvents(workspace string) ([]irminModels.LogEvent, *irmi
 	}
 	return logEvents, apiResp, nil
 }
+
+// FetchLogEventsForRepository retrieves general audit log events for a repository
+func (c *Client) FetchLogEventsForRepository(workspace, repository_id string) ([]irminModels.LogEvent, *irminModels.IrminAPIResponse, error) {
+	var logEvents []irminModels.LogEvent
+	apiResp, err := c.FetchAPI(RequestOptions{
+		Method:   http.MethodGet,
+		Endpoint: fmt.Sprintf("/v1/workspaces/%s/logs?repository_id=%s", workspace, repository_id),
+	}, &logEvents)
+	if err != nil {
+		return nil, nil, fmt.Errorf("fetch log events error: %w", err)
+	}
+	return logEvents, apiResp, nil
+}
