36cb1175a19d99c21eb3727713e71c856ab66b7a
Tim Borovkov <tim.borovkov@icloud.com>
2025-05-14 22:49:22 +0300

    Remove CallSystemDispatch

--- core-api/system.go
+++ core-api/system.go
@@ -13,15 +13,13 @@ import (
 //
 // Usable only with a system token.
 func (c *Client) CallSystemWebhook(
-	queryParams map[string]string,
+	webhookType string,
 	headers map[string]string,
 	body any,
 ) (*irminmodels.IrminAPIResponse, error) {
 	// Build the query params string
 	queryParamsString := url.Values{}
-	for k, v := range queryParams {
-		queryParamsString.Add(k, v)
-	}
+	queryParamsString.Add("type", webhookType)
 
 	// Call the endpoint
 	apiResp, err := c.FetchAPI(RequestOptions{
@@ -33,35 +31,6 @@ func (c *Client) CallSystemWebhook(
 	}, nil)
 	if err != nil {
 		return nil, fmt.Errorf("call system webhook error: %w", err)
-	}
-	return apiResp, nil
-}
-
-// CallSystemDispatch calls the system dispatch endpoint.
-// The body is expected to be an that will be marshaled to JSON.
-//
-// Usable only with a system token.
-func (c *Client) CallSystemDispatch(
-	queryParams map[string]string,
-	headers map[string]string,
-	body any,
-) (*irminmodels.IrminAPIResponse, error) {
-	// Build the query params string
-	queryParamsString := url.Values{}
-	for k, v := range queryParams {
-		queryParamsString.Add(k, v)
-	}
-
-	// Call the endpoint
-	apiResp, err := c.FetchAPI(RequestOptions{
-		Method:      http.MethodPost,
-		Endpoint:    fmt.Sprintf("/v1/system/dispatch?%s", queryParamsString.Encode()),
-		ContentType: "application/json",
-		Body:        body,
-		Headers:     headers,
-	}, nil)
-	if err != nil {
-		return nil, fmt.Errorf("call system dispatch error: %w", err)
 	}
 	return apiResp, nil
 }
