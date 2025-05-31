5c9f8c2ca4667cfddd4f9d971304d8bdfce3a44b
Tim Borovkov <tim.borovkov@icloud.com>
2025-05-31 15:06:15 +0300

    Update invite.go

--- models/invite.go
+++ models/invite.go
@@ -5,7 +5,7 @@ import "time"
 type Invite struct {
 	ID         string     `json:"id"`
 	Email      string     `json:"email"`
-	Role       IrminRole  `json:"role"`
+	RoleID     string     `json:"role_id"`
 	AcceptedAt *time.Time `json:"accepted_at,omitempty"`
 	DeclinedAt *time.Time `json:"declined_at,omitempty"`
 	ExpiresAt  time.Time  `json:"expires_at"`
