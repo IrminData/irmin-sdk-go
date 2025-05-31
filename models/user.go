1cf4c3fc55b5fde02d455bcb97d2c7d95392826c
Tim Borovkov <tim.borovkov@icloud.com>
2025-05-31 15:06:31 +0300

    Update user.go

--- models/user.go
+++ models/user.go
@@ -1,12 +1,12 @@
 package irminmodels
 
 type User struct {
-	ID             string      `json:"id"`
-	FirstName      string      `json:"first_name"`
-	LastName       string      `json:"last_name"`
-	Email          string      `json:"email"`
-	Phone          string      `json:"phone"`
-	Company        string      `json:"company"`
-	ProfilePicture string      `json:"profile_picture"`
-	Roles          []IrminRole `json:"roles,omitempty"`
+	ID             string   `json:"id"`
+	FirstName      string   `json:"first_name"`
+	LastName       string   `json:"last_name"`
+	Email          string   `json:"email"`
+	Phone          string   `json:"phone"`
+	Company        string   `json:"company"`
+	ProfilePicture string   `json:"profile_picture"`
+	RoleIDs        []string `json:"role_ids"`
 }
