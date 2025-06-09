56ab89337b22e9cd2011eb0921f8097c7eabd8b4
Tim Borovkov <tim.borovkov@icloud.com>
2025-06-09 12:20:53 +0300

    Update tag.go

--- models/tag.go
+++ models/tag.go
@@ -1,7 +1,13 @@
 package irminmodels
 
-// Tag represents a repository tag object.
+// Tag represents a workspace tag object for labeling entities.
 type Tag struct {
-	Name string `json:"name"` // Tag name
-	Ref  string `json:"ref"`  // Commit hash referenced in a tag
+	// ID is the unique sqid for the tag
+	ID string `json:"id"`
+	// Name is the name of the tag
+	Name string `json:"name"`
+	// Color is the hex color code for the tag
+	Color string `json:"color"`
+	// Description is the description of the tag, used for tagging entities
+	Description string `json:"description"`
 }
