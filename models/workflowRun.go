22c1e8ac637d50335a7495ecac9a3d37e04ad2b3
Tim Borovkov <tim.borovkov@icloud.com>
2025-07-07 21:27:13 +0300

    Update workflowRun.go

--- models/workflowRun.go
+++ models/workflowRun.go
@@ -8,7 +8,7 @@ type WorkflowRun struct {
 	UpdatedAt       time.Time        `json:"updated_at" validate:"required"`
 	StartedAt       *time.Time       `json:"started_at,omitempty"`
 	FinishedAt      *time.Time       `json:"finished_at,omitempty"`
-	Status          WorkflowStatus   `json:"status" validate:"required,oneof=' paused pending initiating running complete error cancelled'"`
+	Status          WorkflowStatus   `json:"status" validate:"required,oneof=paused pending initiating running complete error cancelled"`
 	TriggeredBy     *ScheduleTrigger `json:"triggered_by,omitempty"`
 	TriggeredByUser *User            `json:"triggered_by_user,omitempty"`
 	WorkflowID      string           `json:"workflow_id" validate:"required,validsqid=workflows"`
