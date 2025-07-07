db5da2b9475a7d60be1bb2122607dbfc58b97b96
Tim Borovkov <tim.borovkov@icloud.com>
2025-07-07 15:30:08 +0300

    Update apiToken.go

--- models/apiToken.go
+++ models/apiToken.go
@@ -3,7 +3,7 @@ package irminmodels
 import "time"
 
 type APIToken struct {
-	ID        string    `json:"id" validate:"required"`
+	ID        string    `json:"id" validate:"required,validsqid=api_tokens"`
 	CreatedAt time.Time `json:"created_at" validate:"required"`
 	UpdatedAt time.Time `json:"updated_at" validate:"required"`
 	Name      string    `json:"name" validate:"required,min=1,max=100"`
