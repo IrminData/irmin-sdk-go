d1db0bf178069c52dcfd630642d95b932d4a4f02
Tim Borovkov <tim.borovkov@icloud.com>
2025-07-07 16:48:10 +0300

    Update schedule.go

--- models/schedule.go
+++ models/schedule.go
@@ -33,25 +33,25 @@ const (
 )
 
 type ScheduleTrigger struct {
-	Type WorkflowTriggerType `json:"type"`
+	Type WorkflowTriggerType `json:"type" validate:"required,oneof=time repository-event workflow-run-event"`
 
 	// Time trigger
-	RRule *string `json:"rrule,omitempty"`
-	Cron  *string `json:"cron,omitempty"`
+	RRule *string `json:"rrule,omitempty" validate:"validrrule,required_if=Type time,required_without=Cron"`
+	Cron  *string `json:"cron,omitempty" validate:"validcron,required_if=Type time,required_without=RRule"`
 
 	// Repository event trigger
-	RepositoryEvent *RepositoryEvent `json:"repository_event,omitempty"`
-	Repository      *string          `json:"repository,omitempty"` // Slug of the repository
-	RepositoryRef   *string          `json:"repository_ref,omitempty"`
+	RepositoryEvent *RepositoryEvent `json:"repository_event,omitempty" validate:"required_if=Type repository-event,oneof=pre-commit post-commit pre-merge post-merge pre-create-branch post-create-branch pre-delete-branch post-delete-branch pre-create-tag post-create-tag pre-delete-tag post-delete-tag"`
+	Repository      *string          `json:"repository,omitempty" validate:"required_with=RepositoryEvent,validslug"` // Slug of the repository
+	RepositoryRef   *string          `json:"repository_ref,omitempty" validate:"required_with=RepositoryEvent"`
 
 	// Workflow run event trigger
-	WorkflowRunEvent *WorkflowRunEvent `json:"workflow_run_event,omitempty"`
-	WorkflowID       *string           `json:"workflow_id,omitempty"` // Sqid of the workflow
+	WorkflowRunEvent *WorkflowRunEvent `json:"workflow_run_event,omitempty" validate:"required_if=Type workflow-run-event,oneof=pre-workflow-run post-workflow-run"`
+	WorkflowID       *string           `json:"workflow_id,omitempty" validate:"required_with=WorkflowRunEvent,validsqid=workflows"` // Sqid of the workflow
 }
 
 type Schedule struct {
-	Triggers    []ScheduleTrigger `json:"triggers"`
-	MaxRetries  int               `json:"max_retries,omitempty"`
-	MaxRuntime  int               `json:"max_runtime,omitempty"`
-	MinInterval int               `json:"min_interval,omitempty"`
+	Triggers    []ScheduleTrigger `json:"triggers" validate:"dive"`
+	MaxRetries  int               `json:"max_retries,omitempty" validate:"min=0,max=10"`
+	MaxRuntime  int               `json:"max_runtime,omitempty" validate:"min=1"`
+	MinInterval int               `json:"min_interval,omitempty" validate:"min=1"`
 }
