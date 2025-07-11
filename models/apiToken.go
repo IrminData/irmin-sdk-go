948a4766679134292720aafdd7c599a1bd06760c
Tim Borovkov <tim.borovkov@icloud.com>
2025-07-12 00:54:03 +0300

    Update apiToken.go

--- models/apiToken.go
+++ models/apiToken.go
@@ -7,6 +7,6 @@ type APIToken struct {
 	CreatedAt time.Time `json:"created_at"      validate:"required"`
 	UpdatedAt time.Time `json:"updated_at"      validate:"required"`
 	Name      string    `json:"name"            validate:"required,min=1,max=100"`
-	Token     string    `json:"token,omitempty" validate:"min=64,validtoken"`
+	Token     *string   `json:"token,omitempty" validate:"min=64,validtoken"`
 	ExpiresAt time.Time `json:"expiry"          validate:"required,gtfield=CreatedAt"`
 }
