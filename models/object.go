4238a13c4d2d5292916b74fc046d7f4e9046df07
Tim Borovkov <tim.borovkov@icloud.com>
2025-07-12 02:44:31 +0300

    Update object.go

--- models/object.go
+++ models/object.go
@@ -19,12 +19,12 @@ type Object struct {
 	RepositorySlug        string            `json:"repository_slug"                   validate:"required,max=100"` // The slug of the repository that contains the object.
 	Ref                   string            `json:"ref"                               validate:"required,max=100"` // The branch or other reference that contains the object.
 	Type                  ObjectType        `json:"type"                              validate:"required,oneof=group structured binary"`
-	ContentType           string            `json:"content_type,omitempty"            validate:"max=100"` // The MIME type of the object content, like "application/json" or "text/plain".
-	PhysicalAddress       string            `json:"physical_address,omitempty"        validate:"uri"`     // The location of the object on the underlying object store. Formatted as a native URI with the object store type as scheme ("s3://...", "gs://...", etc.) Or, in the case of presign=true, will be an HTTP URL to be consumed via regular HTTP GET
-	PhysicalAddressExpiry *int64            `json:"physical_address_expiry,omitempty" validate:"min=0"`   // If present and nonzero, physical_address is a pre-signed URL and will expire at this Unix Epoch time. This will be shorter than the pre-signed URL lifetime if an authentication token is about to expire.
-	SizeBytes             int64             `json:"size_bytes,omitempty"              validate:"min=0"`   // The number of bytes in the object.
-	LastModified          string            `json:"last_modified,omitempty"`                              // The last modified time of the object in RFC3339 format.
-	Metadata              map[string]string `json:"metadata,omitempty"`                                   // Key-value pairs of metadata about the object.
-	Tags                  []Tag             `json:"tags,omitempty"                    validate:"dive"`    // Tags associated with this object.
-	Children              []Object          `json:"children,omitempty"                validate:"dive"`    // If the object is a group, this will contain the children objects.
+	ContentType           string            `json:"content_type,omitempty"            validate:"max=100,excluded_if=Type group"` // The MIME type of the object content, like "application/json" or "text/plain".
+	PhysicalAddress       string            `json:"physical_address,omitempty"        validate:"omitempty,uri"`                  // The location of the object on the underlying object store. Formatted as a native URI with the object store type as scheme ("s3://...", "gs://...", etc.) Or, in the case of presign=true, will be an HTTP URL to be consumed via regular HTTP GET
+	PhysicalAddressExpiry *int64            `json:"physical_address_expiry,omitempty" validate:"omitempty,min=0"`                // If present and nonzero, physical_address is a pre-signed URL and will expire at this Unix Epoch time. This will be shorter than the pre-signed URL lifetime if an authentication token is about to expire.
+	SizeBytes             int64             `json:"size_bytes,omitempty"              validate:"omitempty,min=0"`                // The number of bytes in the object.
+	LastModified          string            `json:"last_modified,omitempty"           validate:"omitempty,datetime"`             // The last modified time of the object in RFC3339 format.
+	Metadata              map[string]string `json:"metadata,omitempty"`                                                          // Key-value pairs of metadata about the object.
+	Tags                  []Tag             `json:"tags,omitempty"                    validate:"dive,omitempty"`                 // Tags associated with this object.
+	Children              []Object          `json:"children,omitempty"                validate:"dive,omitempty"`                 // If the object is a group, this will contain the children objects.
 }
