532aff89de5502ed8a31646ea68623b24cd83c1e
Tim Borovkov <tim.borovkov@icloud.com>
2025-08-02 00:38:42 +0300

    Update editor.go

--- models/editor.go
+++ models/editor.go
@@ -10,20 +10,20 @@ const (
 )
 
 type EditorItem struct {
-	Name         string         `json:"name"               validate:"required,max=255" example:"file.txt"`
-	Path         string         `json:"path"               validate:"required" example:"/path/to/file.txt"`
+	Name         string         `json:"name"               validate:"required,max=255"           example:"file.txt"`
+	Path         string         `json:"path"               validate:"required"                   example:"/path/to/file.txt"`
 	Type         EditorItemType `json:"type"               validate:"required,oneof=file folder" example:"file"`
-	Content      *string        `json:"content,omitempty" example:"This is the content of the file"`
-	Language     *string        `json:"language,omitempty" validate:"required_if=Type file" example:"js"` // js, py, go, etc.
+	Content      *string        `json:"content,omitempty"                                        example:"This is the content of the file"`
+	Language     *string        `json:"language,omitempty" validate:"required_if=Type file"      example:"js"` // js, py, go, etc.
 	Children     []EditorItem   `json:"children,omitempty" validate:"dive,excluded_if=Type file"`
-	LastModified time.Time      `json:"last_modified"      validate:"required" example:"2025-01-15T10:30:00Z"`
+	LastModified time.Time      `json:"last_modified"      validate:"required"                   example:"2025-01-15T10:30:00Z"`
 }
 
 type ScriptResult struct {
-	StructuredResults map[string][]map[string]any `json:"structured_results,omitempty" example:"{\"customers.json\":[{\"name\":\"John\",\"age\":30}]}"` // Parsed resulting structured files
-	HasErrors         bool                        `json:"has_errors,omitempty" example:"false"`
-	Duration          time.Duration               `json:"duration,omitempty" example:"1s"`
-	StartedAt         time.Time                   `json:"started_at,omitempty" example:"2025-01-15T10:30:00Z"`
-	FinishedAt        time.Time                   `json:"finished_at,omitempty" example:"2025-01-15T10:30:00Z"`
-	Logs              []string                    `json:"logs,omitempty"               validate:"dive" example:"[\"Log message 1\",\"Log message 2\"]"`
+	StructuredResults map[string][]map[string]any `json:"structured_results,omitempty" example:"{'customers.json':[{'name':'John','age':30}]}"` // Parsed resulting structured files
+	HasErrors         bool                        `json:"has_errors,omitempty"         example:"false"`
+	Duration          time.Duration               `json:"duration,omitempty"           example:"1s"`
+	StartedAt         time.Time                   `json:"started_at,omitempty"         example:"2025-01-15T10:30:00Z"`
+	FinishedAt        time.Time                   `json:"finished_at,omitempty"        example:"2025-01-15T10:30:00Z"`
+	Logs              []string                    `json:"logs,omitempty"               example:"Log message 1,Log message 2" validate:"dive"`
 }
