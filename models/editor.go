36d36402eddb2e61ecb299359741aa085a7633a8
Tim Borovkov <tim.borovkov@icloud.com>
2025-08-01 23:32:56 +0300

    Update editor.go

--- models/editor.go
+++ models/editor.go
@@ -16,14 +16,14 @@ type EditorItem struct {
 	Content      *string        `json:"content,omitempty" example:"This is the content of the file"`
 	Language     *string        `json:"language,omitempty" validate:"required_if=Type file" example:"js"` // js, py, go, etc.
 	Children     []EditorItem   `json:"children,omitempty" validate:"dive,excluded_if=Type file"`
-	LastModified time.Time      `json:"last_modified"      validate:"required" example:"2023-01-15T10:30:00Z"`
+	LastModified time.Time      `json:"last_modified"      validate:"required" example:"2025-01-15T10:30:00Z"`
 }
 
 type ScriptResult struct {
 	StructuredResults map[string][]map[string]any `json:"structured_results,omitempty" example:"{\"customers.json\":[{\"name\":\"John\",\"age\":30}]}"` // Parsed resulting structured files
 	HasErrors         bool                        `json:"has_errors,omitempty" example:"false"`
 	Duration          time.Duration               `json:"duration,omitempty" example:"1s"`
-	StartedAt         time.Time                   `json:"started_at,omitempty" example:"2023-01-15T10:30:00Z"`
-	FinishedAt        time.Time                   `json:"finished_at,omitempty" example:"2023-01-15T10:30:00Z"`
+	StartedAt         time.Time                   `json:"started_at,omitempty" example:"2025-01-15T10:30:00Z"`
+	FinishedAt        time.Time                   `json:"finished_at,omitempty" example:"2025-01-15T10:30:00Z"`
 	Logs              []string                    `json:"logs,omitempty"               validate:"dive" example:"[\"Log message 1\",\"Log message 2\"]"`
 }
