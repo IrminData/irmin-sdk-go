8b0cb4497c57b818b940835af4ff7b4d105e259c
Tim Borovkov <tim.borovkov@icloud.com>
2025-08-01 23:32:51 +0300

    Update apiToken.go

--- models/apiToken.go
+++ models/apiToken.go
@@ -4,9 +4,9 @@ import "time"
 
 type APIToken struct {
 	ID        string    `json:"id"              validate:"required,validsqid=api_tokens" example:"token_1a2b3c"`
-	CreatedAt time.Time `json:"created_at"      validate:"required"                      example:"2023-01-15T10:30:00Z"`
-	UpdatedAt time.Time `json:"updated_at"      validate:"required"                      example:"2023-12-01T14:22:30Z"`
+	CreatedAt time.Time `json:"created_at"      validate:"required"                      example:"2025-01-15T10:30:00Z"`
+	UpdatedAt time.Time `json:"updated_at"      validate:"required"                      example:"2025-12-01T14:22:30Z"`
 	Name      string    `json:"name"            validate:"required,max=100"              example:"API Token 1"`
 	Token     *string   `json:"token,omitempty" validate:"min=64,validtoken"             example:"1a2b3c4d5e6f7g8h9i0j1k22x3y4z5a6b7c8d9e0"`
-	ExpiresAt time.Time `json:"expiry"          validate:"required,gtfield=CreatedAt"    example:"2023-01-15T10:30:00Z"`
+	ExpiresAt time.Time `json:"expiry"          validate:"required,gtfield=CreatedAt"    example:"2025-01-15T10:30:00Z"`
 }
