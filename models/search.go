71c3e27987332fcec9dd7d3b8cbd7dc97327aff7
Tim Borovkov <tim.borovkov@icloud.com>
2025-08-02 14:34:00 +0300

    Update search.go

--- models/search.go
+++ models/search.go
@@ -32,7 +32,7 @@ type SearchResult struct {
 type SearchFilters struct {
 	Query    string   `json:"query"               validate:"required,min=3"                                                                example:"customer data"`
 	Types    []string `json:"types"               validate:"dive,oneof=workflow repository connection query user repository_object invite" example:"repository,workflow"`
-	Tags     []string `json:"tags"                validate:"validsqid=tags"                                                                example:"tag_8x2m9k4n7p5q,tag_9y3n0l5o8r6s"`
+	Tags     []string `json:"tags"                validate:"dive,validsqid=tags"                                                           example:"tag_8x2m9k4n7p5q,tag_9y3n0l5o8r6s"`
 	OwnerID  *string  `json:"owner_id,omitempty"  validate:"omitempty,validsqid=users"                                                     example:"usr_2k8n9q1m7p3x4z"`
 	DateFrom *string  `json:"date_from,omitempty" validate:"omitempty,datetime"                                                            example:"2025-01-01T00:00:00Z"`
 	DateTo   *string  `json:"date_to,omitempty"   validate:"omitempty,datetime"                                                            example:"2025-12-31T23:59:59Z"`
