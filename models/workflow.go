765f11eac321b133a4084b85936cf6a8304b75a4
Tim Borovkov <tim.borovkov@icloud.com>
2025-12-11 18:03:26 +0200

    Update workflow.go

--- models/workflow.go
+++ models/workflow.go
@@ -45,7 +45,9 @@ type PipelineStage struct {
 	Type          PipelineStageType `json:"type"           validate:"required,oneof=action connection repository,validpipelinestage" example:"repository"`
 
 	// Action stage specific
-	ScriptID *string `json:"script_id,omitempty" validate:"validsqid=scripts,required_if=Type action" example:"scr_8x2m9k4n7p5q"`
+	ExecutableType ActionExecutableType `json:"executable_type,omitempty" validate:"required,oneof=script query,required_if=Type action" example:"script"`
+	ScriptID       *string              `json:"script_id,omitempty" validate:"validsqid=scripts,required_if=Type action,required_if=ExecutableType script" example:"scr_8x2m9k4n7p5q"`
+	QueryID        *string              `json:"query_id,omitempty" validate:"validsqid=queries,required_if=Type action,required_if=ExecutableType query" example:"qry_8x2m9k4n7p5q"`
 
 	// Connection stage specific
 	ConnectionID        *string   `json:"connection_id,omitempty"         validate:"validsqid=connections,required_if=Type connection" example:"conn_8x2m9k4n7p5q"`
