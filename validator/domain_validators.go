5a33ec6d29a803174254f8ca7815b47917386401
Tim Borovkov <tim.borovkov@icloud.com>
2025-12-11 18:59:21 +0200

    wip

--- validator/domain_validators.go
+++ validator/domain_validators.go
@@ -84,11 +84,32 @@ func (v *Validator) validatePipelineStage(fl validator.FieldLevel) bool {
 
 // validateActionPipelineStage validates action-type pipeline stages.
 func (v *Validator) validateActionPipelineStage(parentStruct reflect.Value) bool {
+	executableTypeField := parentStruct.FieldByName("ExecutableType")
 	scriptIDField := parentStruct.FieldByName("ScriptID")
+	queryIDField := parentStruct.FieldByName("QueryID")
 
-	// Action stages must have a valid script ID
-	if !v.validateResourceID(scriptIDField, "scripts") {
-		return false
+	// Get ExecutableType value
+	var executableType string
+	if executableTypeField.IsValid() {
+		executableType = executableTypeField.String()
+	}
+
+	// Validate based on ExecutableType
+	switch executableType {
+	case "script":
+		// Action stages with script executable type must have a valid script ID
+		if !v.validateResourceID(scriptIDField, "scripts") {
+			return false
+		}
+	case "query":
+		// Action stages with query executable type must have a valid query ID
+		if !v.validateResourceID(queryIDField, "queries") {
+			return false
+		}
+	default:
+		// ExecutableType validation is handled by standard tags
+		// If ExecutableType is not set or invalid, let other validators handle it
+		return true
 	}
 
 	return true
