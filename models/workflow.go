6402a86ba4dc0a20c45209049437b078be712b35
Tim Borovkov <tim.borovkov@icloud.com>
2025-12-27 21:10:14 +0200

    Update workflow.go

--- models/workflow.go
+++ models/workflow.go
@@ -48,6 +48,7 @@ const (
 	TransformOpFieldRename   TransformOperationType = "field_rename"
 	TransformOpFieldRemove   TransformOperationType = "field_remove"
 	TransformOpFileRename    TransformOperationType = "file_rename"
+	TransformOpFileRemove    TransformOperationType = "file_remove"
 	TransformOpFormatConvert TransformOperationType = "format_convert"
 )
 
