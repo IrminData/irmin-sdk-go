5a03fd2e525e9bcb2c6c550a265d6184644c357b
Tim Borovkov <tim.borovkov@icloud.com>
2025-12-27 21:28:23 +0200

    Update domain_validators.go

--- validator/domain_validators.go
+++ validator/domain_validators.go
@@ -277,7 +277,7 @@ func (v *Validator) validateValidationPipelineStage(parentStruct reflect.Value) 
 
 // validateTransformPipelineStage validates transform-type pipeline stages.
 //
-//nolint:gocognit // There is no need to refactor this code
+//nolint:gocognit,gocyclo,cyclop // There is no need to refactor this code
 func (v *Validator) validateTransformPipelineStage(parentStruct reflect.Value) bool {
 	transformOperationField := parentStruct.FieldByName("TransformOperation")
 	transformModeField := parentStruct.FieldByName("TransformMode")
